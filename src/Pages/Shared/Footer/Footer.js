b66b267e33835ffce0f886c0bb46e559f36fbe5b
Saklain Mustak <saklainmustak@sakmac.local>
2022-04-16 07:00:43 +0600

    pages and images loaded

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+
+const Footer = () => {
+    const year = '';
+
+    return (
+     <footer>
+         <p><small>copyright  &Copy reserved {year}</small></p>
+     </footer>
+    );
+};
+
+export default Footer;
